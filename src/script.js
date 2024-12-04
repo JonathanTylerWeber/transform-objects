21889001dc3a5917bd643b9480093d430fde03cb
Jonathan Tyler Weber <jonathantweber@gmail.com>
2024-12-04 17:18:11 +0800

    done

--- src/script.js
+++ src/script.js
@@ -1,39 +1,90 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group();
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "red" })
+);
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "green" })
+);
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "blue" })
+);
+cube1.position.set(2, 0, 0);
+cube2.position.set(0, 0, 0);
+cube3.position.set(-2, 0, 0);
+
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh = new THREE.Mesh(geometry, material);
+// // mesh.position.x = 0.7;
+// // mesh.position.y = -0.6;
+// // mesh.position.z = 1;
+
+// mesh.position.set(0.7, -0.6, 1);
+
+// // mesh.scale.x = 2;
+// // mesh.scale.y = 0.5;
+// // mesh.scale.z = 0.5;
+
+// mesh.scale.set(2, 0.5, 0.5);
+
+// // Math.PI is half rotation, 180degrees, 2 * PI is full rotation 360 degrees
+// // Math.PI * 0.5 quarter rotation or divided by 2
+// mesh.rotation.reorder("YXZ");
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 0.25;
+
+// scene.add(mesh);
+
+// axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 /**
  * Camera
  */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// camera.lookAt(new THREE.Vector3(3, 0, 0))
+// camera.lookAt(mesh.position);
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
